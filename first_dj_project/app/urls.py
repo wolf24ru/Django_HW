d48aed1cf6662e87ba78be09056ad5dae860670c
Nikita <wolf24ru@gmail.com>
2021-10-28 14:46:52 +0700

    clear main rep

--- first_dj_project/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, current_item, workdir
-
-# app_name = 'app'
-urlpatterns = [
-    path('', home, name='home'),
-    path('current_item/', current_item, name='current'),
-    path('workdir/', workdir, name='workdir'),
-    ]
