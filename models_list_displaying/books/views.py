543bc4dd6299fe514deedc3b4b7675e8a9041fe2
Nikita <wolf24ru@gmail.com>
2021-11-03 16:43:48 +0700

    create new function

--- models_list_displaying/books/views.py
+++ models_list_displaying/books/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+
+def index(request):
+    return redirect('books')
 
 
 def books_view(request):
     template = 'books/books_list.html'
     context = {}
     return render(request, template, context)
+
+
+def books_date_view(request, pub_date):
+    template = 'books/books_list.html'
