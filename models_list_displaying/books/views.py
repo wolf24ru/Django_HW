facc265c3fa94d19ca9d6d35a078606420d8e924
Nikita <wolf24ru@gmail.com>
2021-11-09 19:50:14 +0700

    Must finish

--- models_list_displaying/books/views.py
+++ models_list_displaying/books/views.py
@@ -1,5 +1,6 @@
+from .models import Book
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
-
 
 def index(request):
     return redirect('books')
@@ -7,9 +8,30 @@ def index(request):
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    context = {
+        'books': Book.objects.all()
+    }
     return render(request, template, context)
 
 
-def books_date_view(request, pub_date):
-    template = 'books/books_list.html'
+def books_date_view(request, pub_date_req):
+    template = 'books/books_date.html'
+    objects_list = Book.objects.all()
+    date_books = []
+    paginator_dict = {}
+    # current_books = objects_list.filter(pub_date=pub_date)
+    for book in objects_list.order_by('pub_date'):
+        paginator_dict.update({book.pub_date: objects_list.filter(pub_date=book.pub_date)})
+        if book.pub_date not in date_books:
+            date_books.append(book.pub_date)
+    #TODO сделать пангинатор из имеющихся данных.
+
+    # date_list = [i.pub_date for i in objects_list]
+    # paginator = Paginator(paginator_dict, 1)
+    # current_page = request.GET.get('page', pub_date_req)
+    # page = paginator.get_page(current_page)
+    context = {
+        # 'page': page,
+        # 'books': page.object_list,
+    }
+    return render(request, template, context)
