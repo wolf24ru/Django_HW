a81af88feeec5a4ba20700aa217e608ab6f79f6e
Nikita <wolf24ru@gmail.com>
2021-11-12 10:14:42 +0700

    Prepared for new works
    deleted all old files

--- work_with_database/phones/admin.py
+++
@@ -1,7 +0,0 @@
-from .models import Phone
-from django.contrib import admin
-
-@admin.register(Phone)
-class PhoneAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
-    list_display = ('name', 'slug')
