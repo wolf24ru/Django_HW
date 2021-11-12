b67c5592165c49ded2544fa3b73ccec3da3df9da
Nikita <wolf24ru@gmail.com>
2021-11-12 15:12:42 +0700

    add views function. completely done

--- school/views.py
+++ school/views.py
@@ -6,10 +6,12 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
+    students = Student.objects.prefetch_related('teacher').order_by('group', 'name')
+    context = {
+        'students': students,
+               }
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
 
     return render(request, template, context)
