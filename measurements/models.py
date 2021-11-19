0aedde915746f0bae5dd223cc299094eb52ec6c8
Nikita <wolf24ru@gmail.com>
2021-11-19 14:40:24 +0700

    add field picture

--- measurements/models.py
+++ measurements/models.py
@@ -15,28 +15,13 @@ class TimestampFields(models.Model):
 
 class Project(TimestampFields):
     """Объект на котором проводят измерения."""
-
     name = models.TextField()
     latitude = models.FloatField()
     longitude = models.FloatField()
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
-    #
-    # class Meta:
-    #     abstract = True
 
-class Measurement(models.Model):
+
+class Measurement(TimestampFields):
     """Измерение температуры на объекте."""
-
     value = models.FloatField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
+    picture = models.ImageField(null=True)
