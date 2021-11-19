b54a06201fa9e8bc73dfd5831aa56b5ff1a2609c
Nikita <wolf24ru@gmail.com>
2021-11-19 14:02:18 +0700

    add import modules

--- simple_crud/urls.py
+++ simple_crud/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from measurements.api_view import ProjectViewSet, MeasurementViewSet
+from rest_framework import routers
 
 # TODO: настройте роутер и подключите `ProjectViewSet` и `MeasurementViewSet`
 
