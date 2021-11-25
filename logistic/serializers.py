4d77eac61a7da2b04c53df813e653e1e12eb332c
Nikita <wolf24ru@gmail.com>
2021-11-25 18:04:44 +0700

    redefine 'create' and 'update' metods

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from logistic.models import Product, Stock, StockProduct
 
 
@@ -15,8 +14,6 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    # PrimaryKeyRelatedField
-    product = serializers.IntegerField()
     quantity = serializers.IntegerField()
     price = serializers.DecimalField(max_digits=18, decimal_places=2)
 
@@ -29,33 +26,33 @@ class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ["address", "positions"]
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
         # создаем склад по его параметрам
         stock = super().create(validated_data)
-
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for position in positions:
+            StockProduct.objects.create(stock=stock,
+                                        product=position['product'],
+                                        quantity=position['quantity'],
+                                        price=position['price'])
         return stock
 
     def update(self, instance, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
 
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for position in positions:
+            StockProduct.objects.update_or_create(pk=instance.pk, defaults={
+                'stock': stock,
+                'product': position['product'],
+                'quantity': position['quantity'],
+                'price': position['price']
+            })
         return stock
-
-    class Meta:
-        model = Stock
-        fields = ["address", "positions"]
