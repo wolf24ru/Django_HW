18ab6b1560967a95e7ad3c08e3cbc0cfeb12aa77
Nikita <wolf24ru@gmail.com>
2021-11-25 18:05:42 +0700

    add number of pages in paginator

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -121,6 +121,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 5
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
