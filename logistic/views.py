5386a69a26d12338b82f2404b36948cc446679ff
Nikita <wolf24ru@gmail.com>
2021-11-25 18:06:03 +0700

    add filter in Stock

--- logistic/views.py
+++ logistic/views.py
@@ -1,8 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
-from logistic.serializers import ProductSerializer, StockSerializer
+from logistic.serializers import ProductSerializer, StockSerializer, StockFilter
 
 
 class ProductViewSet(ModelViewSet):
@@ -16,5 +18,7 @@ class ProductViewSet(ModelViewSet):
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['products']
+    pagination_class = LimitOffsetPagination
     # при необходимости добавьте параметры фильтрации
