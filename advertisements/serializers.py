b9e96f3c03c8e7eb47238845f1736416a684faa6
Nikita <wolf24ru@gmail.com>
2021-11-30 16:40:27 +0700

    update validate
    users can have only 10 advertisements with status OPEN.

--- advertisements/serializers.py
+++ advertisements/serializers.py
@@ -22,12 +22,11 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Advertisement
-        fields = ('id', 'title', 'description', 'creator',
-                  'status', 'created_at', )
+        fields = ('id', 'title', 'description',
+                  'creator', 'status', 'created_at')
 
     def create(self, validated_data):
         """Метод для создания"""
-
         # Простановка значения поля создатель по-умолчанию.
         # Текущий пользователь является создателем объявления
         # изменить или переопределить его через API нельзя.
@@ -39,7 +38,14 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
-
-        # TODO: добавьте требуемую валидацию
-
-        return data
+        count_status = len(Advertisement.objects.all().filter(status='OPEN', creator=self.context['request'].user))
+        error_msg = 'Один пользователь может иметь только 10 одновременно открытых объявлений'
+        match self.context['view'].action:
+            case 'partial_update':
+                if count_status <= 9 or data['status'] == 'CLOSED':
+                    return data
+                raise ValueError(error_msg)
+            case 'create':
+                if count_status <= 9:
+                    return data
+                raise ValueError(error_msg)
