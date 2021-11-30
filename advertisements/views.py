5a1d795ef89c815d749ba524b4a2496b19a8b030
Nikita <wolf24ru@gmail.com>
2021-11-30 16:36:35 +0700

    add permissions for owner and admin

--- advertisements/views.py
+++ advertisements/views.py
@@ -11,5 +11,5 @@ class AdvertisementViewSet(ModelViewSet):
     def get_permissions(self):
         """Получение прав для действий."""
         if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
+            return [IsAuthenticated(), IsOwnerOrReadOnly(), IsAdminUser()]
         return []
