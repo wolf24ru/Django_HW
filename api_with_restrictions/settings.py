01cfce47d08112f75e398b5a54243f76e258d3b8
Nikita <wolf24ru@gmail.com>
2021-11-30 16:32:07 +0700

    db connection

--- api_with_restrictions/settings.py
+++ api_with_restrictions/settings.py
@@ -97,6 +97,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_classified_ads',
+        'USER': 'netology',
+        'PASSWORD': '12345678',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
