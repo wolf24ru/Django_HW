e54eeed1401ab36cce45fce23020afe6dbea5dd1
Nikita <wolf24ru@gmail.com>
2021-11-30 16:31:53 +0700

    add throttling
    throttling for users 20/min,
    throttling for anons 10/min,

--- api_with_restrictions/settings.py
+++ api_with_restrictions/settings.py
@@ -76,7 +76,15 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '20/minute',
+        'anon': '10/minute'
+    }
 }
 
 WSGI_APPLICATION = 'api_with_restrictions.wsgi.application'
