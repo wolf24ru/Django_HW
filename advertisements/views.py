60b88c9efee46c57bfaa7e196ab6ece699b3d43e
Nikita <wolf24ru@gmail.com>
2021-12-02 09:17:00 +0700

    fix admin permission

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,11 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
-from advertisements.permissions import IsOwnerOrReadOnly
+from advertisements.permissions import IsOwnerOrReadOnly, IsAdminUserOrReadOnly
 from advertisements.serializers import AdvertisementSerializer
 
 
@@ -17,9 +17,10 @@ class AdvertisementViewSet(ModelViewSet):
     filter_class = AdvertisementFilter
     filterset_fields = ['created_at', 'updated_at', 'status', 'creator']
     ordering_fields = ['created_at', 'updated_at', 'status']
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly | IsAdminUserOrReadOnly]
 
-    def get_permissions(self):
-        """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated(), IsOwnerOrReadOnly(), IsAdminUser()]
-        return []
+    # def get_permissions(self):
+    #     """Получение прав для действий."""
+    #     if self.action in ["create", "update", "partial_update"]:
+    #         return [IsAuthenticated(), IsOwnerOrReadOnly(), IsAdminUserOrReadOnly()]
+    #     return []
